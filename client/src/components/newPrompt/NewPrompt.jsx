96e9e59f4a701283d35ff08c1d864c65f07c8323
unknown <medisaagum@.gmail.com>
2025-01-24 19:43:29 +0700

    membuat bagian upload image sampai bisa diproses

--- client/src/components/newPrompt/NewPrompt.jsx
+++ client/src/components/newPrompt/NewPrompt.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
-import { useRef, useEffect } from 'react'
+import { useRef, useEffect, useState } from 'react'
 import './newPrompt.css'
 import Upload from '../upload/Upload'
+import { IKImage } from 'imagekitio-react'
 
 const NewPrompt = () => {
+
+  const [img, setImg] = useState({
+    isLoading:false,
+    error:"",
+    dbData:{},
+  })
+
     const endRef=useRef(null)
   
     useEffect(() =>{
@@ -12,9 +20,18 @@ const NewPrompt = () => {
     
   return (
     <>
+    {img.isLoading && <div className=''>Loading...</div>}
+    {img.dbData?.filePath && (
+      <IKImage
+      urlEndpoint={import.meta.env.VITE_IMAGE_KIT_ENDPOINT}
+      path={img.dbData?.filePath}
+      width="200"
+
+      />
+    )}
     <div className='endChat' ref={endRef}></div>
     <form className='newForm'>
-    <Upload/>
+    <Upload setImg={setImg}/>
     <input id='file' type='file' multiple={false} hidden/>
     <input type='text' placeholder='Tanya lah...'/>
     <button>
