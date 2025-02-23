44c3a5caca4e7ef7c1c85fac94ddaf4132a84ba2
unknown <medisaagum@.gmail.com>
2025-02-23 16:48:50 +0700

    membuat bagian explore dan sedikit perubahan style pada web

--- client/src/routes/ekplore/Explore.jsx
+++ client/src/routes/ekplore/Explore.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import React from "react";
+import "./explore.css";
 
 const Explore = () => {
   return (
-    <div>
-      Halooo
+    <div className="Explore">
+      <h1>Tentang RAKYAT</h1>
+      <h4>
+        Riset dan Analitik Kinerja serta Akuntabilitas Transparansi <span className="bold">(RAKYAT) </span>
+        adalah platform berbasis Generative AI yang dirancang untuk melindungi
+        proses pengadaan dari inefisiensi dan potensi kecurangan. Kami
+        menghadirkan solusi berbasis data yang cerdas dan transparan, memastikan
+        setiap langkah dalam pengadaan berjalan secara akuntabel, efisien, dan
+        sesuai dengan prinsip tata kelola yang baik. Dengan teknologi AI yang
+        canggih, RAKYAT menganalisis pola, mendeteksi anomali, dan memberikan
+        wawasan strategis untuk membantu pengambil keputusan meningkatkan
+        efisiensi serta mencegah penyimpangan. Kami percaya bahwa keterbukaan
+        dan inovasi adalah kunci untuk menciptakan ekosistem pengadaan yang
+        lebih adil dan berdaya guna bagi semua pihak.
+      </h4>
+      <p>
+        <span>RAKYAT</span> – Bersama, kita wujudkan pengadaan yang lebih bersih, cerdas, dan
+        transparan.
+      </p>
     </div>
-  )
-}
+  );
+};
 
-export default Explore
+export default Explore;
